981f625775484820bc7cb9767f3672393a5cfa95
Mcc <cucyzx@gmail.com>
2023-11-12 03:07:53 -0500

    add particle and sound

---
+++ Assets/SoundAndParticleController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class SoundAndParticleController : MonoBehaviour
+{
+    public ParticleSystem particleSystem; // Assign your particle system in the inspector
+    public AudioSource audioSource;       // Assign your audio source in the inspector
+
+    // Call this function to start playing sound and particles
+    public void PlaySoundAndParticles()
+    {
+        if (particleSystem != null && audioSource != null)
+        {
+            particleSystem.gameObject.SetActive(true); // Make sure the particle system is active
+            particleSystem.Play(); // Start the particle system
+            audioSource.Play();    // Start playing the sound
+
+            // Call DeactivateParticleSystemAfterSound in the length of the audio clip
+            Invoke("DeactivateParticleSystemAfterSound", audioSource.clip.length);
+        }
+    }
+
+    // Function to deactivate the particle system
+    private void DeactivateParticleSystemAfterSound()
+    {
+        Debug.Log("stop audio and particles");
+        if (particleSystem != null)
+        {
+            particleSystem.gameObject.SetActive(false); // Deactivate the particle system GameObject
+        }
+    }
+}
